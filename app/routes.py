49e4f143bfeedbfd1aeb6cc056db22142d98036d
Tum99 <faithtum92@gmail.com>
2025-05-10 00:41:06 +0300

    Combines login and sign up styles in the new auth.css. Adds new login.html file

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/signup')
 def signup():
     return render_template('signup.html')
+
