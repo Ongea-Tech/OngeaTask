f84c22defae8968847a5753c86038e3a948d3494
Tum99 <faithtum92@gmail.com>
2025-05-09 23:51:30 +0300

    login and signup pages

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
