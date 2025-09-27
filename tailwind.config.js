6c336e63a4a002a4b4f9d0fea29205f1585a2d18
Mercy Tum <mercytum@gmail.com>
2025-09-27 12:58:20 -0400

    integrating Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+    "./app/static/**/*.js",
+    "./**/*.py"
+  ],
+  theme: { extend: {} },
+  darkMode: "class",
+  plugins: []
+}
