9d36a099f67e49884ce4f341e5feb7aa87ade82c
Mercy Tum <mercytum@gmail.com>
2026-04-04 03:45:28 -0400

    playwright ci with github actions

---
+++ tests/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('has get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect link to be visible on the page.
+  await page.getByRole('link', { name: 'GET STARTED' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+});
