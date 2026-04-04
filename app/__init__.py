9134dd86b8b21faad258bcb873a4245dd6f9eb42
Mercy Tum <mercytum@gmail.com>
2026-04-04 04:12:52 -0400

    update init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,56 +2,51 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
-from flask_mail import Mail 
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
 
 db = SQLAlchemy()
 mail = Mail()
 migrate = Migrate()
 login_manager = LoginManager()
+
 def create_app():
-    load_dotenv()  # Loads variables from .env
+    load_dotenv()
 
     app = Flask(__name__)
-    app.secret_key = 'dev-secret-key'
+    app.secret_key = os.getenv('SECRET_KEY', 'dev-secret-key')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", 'sqlite:///test.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///test.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    app.config.update(
+        MAIL_SERVER=os.getenv('MAIL_SERVER', 'smtp.example.com'),
+        MAIL_PORT=int(os.getenv('MAIL_PORT', 587)),
+        MAIL_USE_TLS=os.getenv('MAIL_USE_TLS', 'True') == 'True',
+        MAIL_USERNAME=os.getenv('MAIL_USERNAME', 'test@example.com'),
+        MAIL_PASSWORD=os.getenv('MAIL_PASSWORD', 'testpassword'),
+    )
 
     db.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'  # Redirect to login page if not authenticated
-    
+    login_manager.login_view = 'auth.login'
 
-    # Import routes after app is created to avoid circular import
     from app.routes import routes
     from app.api_routes import api
     from app.auth_routes import auth
-
-    app.config.update(
-    MAIL_SERVER=os.getenv('MAIL_SERVER', 'smtp.example.com'),
-    MAIL_PORT=int(os.getenv('MAIL_PORT', 587)),
-    MAIL_USE_TLS=os.getenv('MAIL_USE_TLS', 'True') == 'True',
-    MAIL_USERNAME=os.getenv('MAIL_USERNAME', 'test@example.com'),
-    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD', 'testpassword'),
-    
-)
+    from app.models import User
 
     app.register_blueprint(routes)
     app.register_blueprint(api)
-    app.register_blueprint(auth, url_prefix = '/auth')
-
-    from app.models import User 
+    app.register_blueprint(auth, url_prefix='/auth')
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # Create tables
     with app.app_context():
         db.create_all()
 
