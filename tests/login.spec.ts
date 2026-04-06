294402d67a8a8b786922960aa7c1e89315aa0153
Tum99 <faithtum92@gmail.com>
2026-04-06 19:46:19 +0300

    Update tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('user can log in successfully', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5000/auth/login');
+  await page.goto('/auth/login');
 
   await page.fill('input[name="username"]', 'Tum');
   await page.fill('input[name="password"]', 'whoisthis');
   await page.getByRole('button', {name:'log in'}).click();
 
-  await expect(page).toHaveURL('http://127.0.0.1:5000/');
+  await expect(page).toHaveURL('/');
   await expect(page.getByText('Ongoing Tasks')).toBeVisible();
 });
 
 test('user can create task successfully', async ({ page }) => {
-  await page.goto('http://127.0.0.1:5000/');
+  await page.goto('/');
 
   await page.fill('input[name="username"]', 'Tum');
   await page.fill('input[name="password"]', 'whoisthis');
