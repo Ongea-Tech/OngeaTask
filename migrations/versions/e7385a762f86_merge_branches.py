894490d5beed77901c67b06aa0c4c16ecfea399f
Mercy Tum <mercytum@gmail.com>
2026-05-25 02:42:14 -0400

    fix migration issue

---
+++ migrations/versions/e7385a762f86_merge_branches.py
@@ -0,0 +1,24 @@
+"""merge branches
+
+Revision ID: e7385a762f86
+Revises: 7d7126740a4f, 92e494566c59
+Create Date: 2026-05-25 02:36:15.260629
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e7385a762f86'
+down_revision = ('7d7126740a4f', '92e494566c59')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
